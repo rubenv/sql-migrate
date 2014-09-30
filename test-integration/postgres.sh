e2f0e8a41d3ebd221b14a852fd74765a0189068a
Ruben Vermeersch <ruben@rocketeer.be>
2014-09-30 09:50:08 +0200

    Add MySQL test.

--- test-integration/postgres.sh
+++ test-integration/postgres.sh
@@ -5,9 +5,18 @@ export PATH=$PATH:$HOME/gopath/bin
 
 set -ex
 
+# PostgreSQL
 PG_OPTIONS="-config=test-integration/dbconfig.yml -env postgres"
 sql-migrate status $PG_OPTIONS
 sql-migrate up $PG_OPTIONS
 sql-migrate down $PG_OPTIONS
 sql-migrate redo $PG_OPTIONS
 sql-migrate status $PG_OPTIONS
+
+# MySQL
+M_OPTIONS="-config=test-integration/dbconfig.yml -env mysql"
+sql-migrate status $M_OPTIONS
+sql-migrate up $M_OPTIONS
+sql-migrate down $M_OPTIONS
+sql-migrate redo $M_OPTIONS
+sql-migrate status $M_OPTIONS
