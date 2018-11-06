29a60465cdf3c420f0b704b86bf2798540988db1
Ruben Vermeersch <ruben@rocketeer.be>
2018-11-06 13:11:59 +0100

    Revert "Split integration test to investigate output during CI"
    This reverts commit e84cf097b52790c1dccd394df24484f7a008c86e.

--- test-integration/mysql-flag.sh
+++ test-integration/mysql-flag.sh
@@ -5,9 +5,6 @@ export PATH=$PATH:$HOME/gopath/bin
 
 OPTIONS="-config=test-integration/dbconfig.yml -env mysql_noflag"
 
-output=$(mktemp $TMPDIR/mysql-flag.XXXXXX)
-
 set -ex
 
-sql-migrate status $OPTIONS | tee $output
-cat $output | grep -q "Make sure that the parseTime option is supplied"
+sql-migrate status $OPTIONS | grep -q "Make sure that the parseTime option is supplied"
