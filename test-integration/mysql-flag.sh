8ad8837b7b46612bc65cefe9a81a7d34e816aac6
Nikita Bolotov <nikita.bolotov@betterme.world>
2025-04-15 18:45:54 +0300

    test fixed

--- test-integration/mysql-flag.sh
+++ test-integration/mysql-flag.sh
@@ -7,4 +7,5 @@ OPTIONS="-config=test-integration/dbconfig.yml -env mysql_noflag"
 
 set -ex
 
-sql-migrate status $OPTIONS | grep -q "Make sure that the parseTime option is supplied"
+sql-migrate status $OPTIONS 2>&1 | grep -q "Make sure that the parseTime option is supplied"
+
